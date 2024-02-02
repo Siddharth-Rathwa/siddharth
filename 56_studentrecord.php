087fdf9ca9d585e7003ab504664f19540d69aea9
Siddharth-Rathwa <siddharthrathwa22@gmail.com>
2024-02-02 11:46:32 +0530

    add a new file

---
+++ 56_studentrecord.php
@@ -0,0 +1,54 @@
+<?php
+    // error_reporting(0);
+    $conn=mysqli_connect("localhost", "root","", "dbstudentrecord");
+    if(!$conn){
+        die ("connection failed". mysqli_connect_error());
+    }
+    echo "connection successfully";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Record</title>
+</head>
+<body>
+    <form action="" enctype="multipart/form-data" name="form" method="post">
+        <table>
+            <tr>
+                <td>Student Id</td>
+                <td><input type="text" name="Studentid" required></td>
+            </tr>
+            <tr>
+                <td>Student Image</td>
+                <td><input type="file" name="Studentimage" required></td>
+            </tr>
+            <tr>
+                <td>Student Name</td>
+                <td><input type="text" name="Studentname" required></td>
+            </tr>
+            <tr>
+                <td>Student Gr.no</td>
+                <td><input type="text" name="Studentgrno" required></td>
+            </tr>
+            <tr>
+                <td>Standard</td>
+                <td><input type="text" name="Studentstandard" required></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="submit" value="submit"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
+<?php
+    if(isset($_POST['submit'])){
+        $Studentid=$_POST['Studentid'];
+        $Studentname=$_POST['Studentname'];
+        $Studentgrno=$_POST['Studetngrno'];
+        $Standard=$_POST['Studentstandard']
+    }
+?>
